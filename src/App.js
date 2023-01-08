6d1f1e9f5e4f9ac4667f14dc84918c69ceae04fa
ThomasThePupper <70729646+DenmarkLouie@users.noreply.github.com>
2023-01-08 16:50:26 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import * as React from "react";
-import { Button } from "@mui/material";
+import { Alert, Button, Grid, Paper, TextField } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
+import { Box, Stack } from "@mui/system";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Box
+        sx={{
+          width: 800,
+          height: 400,
+        }}
+      >
+        <Paper elevation={4}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2} direction={"column"}>
+              <TextField defaultValue="test" {...register("example")} />
+
+              <TextField {...register("exampleRequired", { required: true })} />
+
+              {errors.exampleRequired && (
+                <Alert severity="warning">This field is required</Alert>
+              )}
+
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            </Stack>
+          </form>
+        </Paper>
+      </Box>
+    </Grid>
   );
 }
-
 export default App;
